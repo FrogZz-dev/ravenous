77175fa097f6b469909349fa48cd8ddce58f6a19
FrogZz-dev <71564463+FrogZz-dev@users.noreply.github.com>
2020-09-25 20:34:30 +0200

    add Yelp api comunnication

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -14,6 +14,7 @@ class SearchBar extends Component {
         this.handleTermChange = this.handleTermChange.bind(this);
         this.handleLocationChange = this.handleLocationChange.bind(this);
         this.handleSearch = this.handleSearch.bind(this);
+        this.handleEnterSearch = this.handleEnterSearch.bind(this);
 
         this.sortByOptions = {
             'Best Match': 'best_match',
@@ -27,6 +28,7 @@ class SearchBar extends Component {
         this.setState({
             sortBy: sortByOption
         });
+        this.props.submitSearch(this.state.term, this.state.location, this.state.sortBy);
     }
 
     handleTermChange(event) {
@@ -46,6 +48,12 @@ class SearchBar extends Component {
         event.preventDefault();
     }
 
+    handleEnterSearch(event) {
+        if (event.key === 'Enter') {
+            this.props.submitSearch(this.state.term, this.state.location, this.state.sortBy);
+        }
+    }
+
     renderSortByOption() {
         return Object.keys(this.sortByOptions).map(key => {
             let sortValue = this.sortByOptions[key];
@@ -56,7 +64,7 @@ class SearchBar extends Component {
 
     render() {
         return (
-            <div className="SearchBar">
+            <div onKeyPress={this.handleEnterSearch} className="SearchBar">
                 <div className="SearchBar-sort-options">
                     <ul>
                         {this.renderSortByOption()}
